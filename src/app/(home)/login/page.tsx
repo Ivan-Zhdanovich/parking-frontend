2d86c4cbf7cfa8955bc783eec412ea6fd3c235ba
Zhdanovich <Zhiv80@mail.ru>
2025-05-08 13:13:50 +0300

    Feat: add login image and styles, create new component

--- src/app/(home)/login/page.tsx
+++ src/app/(home)/login/page.tsx
@@ -1,11 +1,16 @@
 import styles from "./loginPage.module.scss";
 import SignIn from "@/app/components/signIn/signIn";
+import login from "../../../assets/pages/loginPage/login.png";
+import Image from "next/image";
 
 export default function Login() {
 	return (
 		<>
 			<h1 className={styles.loginTitle}>Войти в приложение</h1>
-			<SignIn />
+			<div className={styles.loginFormWrapper}>
+				<SignIn />
+				<Image className={styles.loginFormImage} src={login} alt="safe code panel" />
+			</div>
 		</>
 	);
 }
---
+++ src/app/components/schedulerCalendar/schedulerCalendar.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+import DateTimePicker from "react-datetime-picker";
+import "react-datepicker/dist/react-datepicker.css";
+
+export default function SchedulerCalender() {
+	return <DateTimePicker locale={"ru"} />;
+}
