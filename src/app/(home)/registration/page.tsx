8455b50096e5e831e3a9768c5803658f2de313c8
Zhdanovich <Zhiv80@mail.ru>
2025-05-08 10:58:21 +0300

    Feat: add registration form image and styles

--- src/app/(home)/registration/page.tsx
+++ src/app/(home)/registration/page.tsx
@@ -1,11 +1,16 @@
 import Register from "@/app/components/register/register";
+import Image from "next/image";
+import registration from "../../../assets/pages/registrationPage/registration.png";
 import styles from "./registrationPage.module.scss";
 
 export default function Registration() {
 	return (
 		<>
 			<h1 className={styles.registrationTitle}>Зарегистрироваться</h1>
-			<Register />
+			<div className={styles.registrationFormWrapper}>
+				<Register />
+				<Image className={styles.registrationFormImage} src={registration} alt="registration form" />
+			</div>
 		</>
 	);
 }
--- src/app/components/parkingSpot/parkingSpot.tsx
+++ src/app/components/parkingSpot/parkingSpot.tsx
@@ -4,11 +4,9 @@ import styles from "./parkingSpot.module.scss";
 
 export default function ParkingSpot({ id, location, onParkingClick }: IParking) {
 	return (
-		<div className={styles.parkingSpotContainer}>
-			<div role="button" onClick={() => onParkingClick()}>
-				<p className={styles.parkingSpotData}>{id}</p>
-				<p className={styles.parkingSpotData}>{location}</p>
-			</div>
+		<div className={styles.parkingSpotContainer} role="button" onClick={() => onParkingClick()}>
+			<p className={styles.parkingSpotData}>{id}</p>
+			<p className={styles.parkingSpotData}>{location}</p>
 		</div>
 	);
 }
